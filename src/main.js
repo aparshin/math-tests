b1d72f5bded26c1f8b8a16bee0644e446d607ac7
Alexander Parshin <parshin.alexander@gmail.com>
2016-09-03 19:55:06 +0300

    Handle server errors during initialization. Fixes #2

--- src/main.js
+++ src/main.js
@@ -9,41 +9,43 @@ import Config from './config'
 
 import $ from 'jquery'
 
+let start = function(userInfo) {
+    let store = createStore(reducer, {
+        mode: 'SELECT_TEST',
+        username: userInfo ? userInfo.username : null
+    });
+
+    store.subscribe(() => {
+        let state = store.getState()
+        if (state.needSendResults) {
+            $.ajax(Config.get().baseUrl + 'add', {
+                xhrFields: {
+                    withCredentials: true
+                },
+                data : JSON.stringify(state.series),
+                contentType : 'application/json',
+                type : 'POST'
+            })
+
+            store.dispatch({
+                type: 'SENT_RESULTS'
+            })
+        }
+    })
+
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+      document.getElementById('example')
+    )
+}
+
 Config.load().then(() => {
     $.ajax({
         url: Config.get().baseUrl + 'login',
         xhrFields: {
             withCredentials: true
         }
-    }).then((userInfo) => {
-        let store = createStore(reducer, {
-            mode: 'SELECT_TEST',
-            username: userInfo ? userInfo.username : null
-        });
-
-        store.subscribe(() => {
-            let state = store.getState()
-            if (state.needSendResults) {
-                $.ajax(Config.get().baseUrl + 'add', {
-                    xhrFields: {
-                        withCredentials: true
-                    },
-                    data : JSON.stringify(state.series),
-                    contentType : 'application/json',
-                    type : 'POST'
-                })
-
-                store.dispatch({
-                    type: 'SENT_RESULTS'
-                })
-            }
-        })
-
-        render(
-            <Provider store={store}>
-                <App />
-            </Provider>,
-          document.getElementById('example')
-        )
-    })
+    }).then(start, start.bind(null, null))
 })
